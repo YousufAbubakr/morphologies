f431ac056cebba42fadfe17debe97de690937314
YousufAbubakr <yousufabubakr123@berkeley.edu>
2025-12-10 13:54:39 -0800

    re-organizing subjects data structure

---
+++ src/utils/getLevelsInformation.m
@@ -0,0 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Grace O'Connell Biomechanics Lab, UC Berkeley Department of Mechanical
+% Engineering - Etchverry 2162
+%
+% File: getLevelsInformation.m
+% Author: Yousuf Abubakr
+% Project: Morphologies
+% Last Updated: 12-10-2025
+%
+% Description: getting the file paths, level names, and number of 
+% levels associated with each subjects' vertebrae and discs given the
+% user-defined settings
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
